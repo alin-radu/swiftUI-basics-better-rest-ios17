faed3bf5e5f493bc27943b9caefd16d29d3f283b
Alin RADU <alin.radu.cristian@gmail.com>
2024-02-05 20:33:25 +0200

    Initial Commit

---
+++ BetterRest-ios17/BetterRest_ios17App.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRest_ios17App.swift
+//  BetterRest-ios17
+//
+//  Created by Alin RADU on 05.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRest_ios17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
